97205acf833ae13d0d8ebaf1cc56e409494fb847
Anjum Ara <anjumara0711@gmail.com>
2022-03-30 12:06:59 +0530

    login and signup

--- royal_brothers/src/Components/Login/Login.jsx
+++ royal_brothers/src/Components/Login/Login.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
+import './login.css'
+import {Link} from 'react-router-dom'
 
 export const Login = () => {
   return (
-    <div>Login</div>
+    <div className="login">
+      <p className="head">Rent . Ride . Explore</p>
+
+      <div className="box">
+
+        <div className="flex">
+          <p>LOGIN</p>
+          <p><Link to="/register" style={{textDecoration:'none',color:'black'}}>SIGN UP</Link></p>
+        </div>
+
+        <p className="ph">Phone</p>
+        <div className="phone">
+          <input type="number" placeholder="+91" />
+          <input type="number" />
+        </div>
+        
+        <input className="inp" type="password" placeholder="Password"/>
+
+        <div className="forget">
+          <div>
+            <input type="checkbox"/> Remember password?
+          </div>
+          <a href="##">Forgot password?</a>
+        </div>
+
+        <button className="btn">Login with Password</button>
+        <p>OR</p>
+        <button>Login with OTP</button>
+
+      </div>
+    </div>
   )
 }
--- royal_brothers/src/Components/Navbar/Navbar.jsx
+++ royal_brothers/src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+      Navbar
+    <Link to={"/store"}>Store</Link>  
+    <Link to={"/login"}>Login</Link>
+    <Link to={"/signup"}>SignUp</Link>  
+    </div>
   )
 }
