75359bef3fa2dc9e4168f7198a4253ab9f630e0c
Nitish8800 <nitishfea120784@gmail.com>
2022-04-01 13:07:18 +0530

    nitish product bike page

--- royal_brothers/src/Components/Routes/Routes.jsx
+++ royal_brothers/src/Components/Routes/Routes.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import {Routes as ROUTES, Route} from 'react-router-dom'
-import { Footer } from '../Footer/Footer'
-import { Home } from '../Home/Home'
-import { Login } from '../Login/Login'
-import { Navbar } from '../Navbar/Navbar'
-import { NotFound } from '../NotFound/NotFound'
-import { SignUp } from '../SignUp/SignUp'
-import {Store} from '../Store/Store'
-import { Tariff } from '../Tariff/Tariff'
-import { ProductPage } from '../Product/product'
+import React from "react";
+import { Routes as ROUTES, Route } from "react-router-dom";
+import { Footer } from "../Footer/Footer";
+import { Home } from "../Home/Home";
+import { Login } from "../Login/Login";
+import { Navbar } from "../Navbar/Navbar";
+import { NotFound } from "../NotFound/NotFound";
+import { SignUp } from "../SignUp/SignUp";
+import { Store } from "../Store/Store";
+import { Tariff } from "../Tariff/Tariff";
+import { Product } from "../Product/product";
 
 export const Routes = () => {
   return (
@@ -19,12 +19,12 @@ export const Routes = () => {
         <Route path="/store" element={<Store />} />
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/register" element={<SignUp />} />
-        <Route exact path="/tariff" element={<Tariff/>} />
-        <Route exact path="/product" element={<ProductPage />} />
+        <Route exact path="/tariff" element={<Tariff />} />
+        <Route exact path="/product" element={<Product />} />
         {/* {Write all the routes here} */}
         <Route exact path="*" element={<NotFound />} />
       </ROUTES>
       <Footer />
     </>
   );
-}
+};
