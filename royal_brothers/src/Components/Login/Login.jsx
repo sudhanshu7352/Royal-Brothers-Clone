90e4695cf163a53747e943cfb7623002e48c236a
divyansh <sikarwar.divyansh.13@gmail.com>
2022-04-01 21:49:10 +0530

    recaptcha added still pending,login signup css fix

--- royal_brothers/src/Components/Login/Login.jsx
+++ royal_brothers/src/Components/Login/Login.jsx
@@ -1,17 +1,33 @@
 import React from 'react'
 import './Login.css'
 import {Link} from 'react-router-dom'
+import ReCAPTCHA from "react-google-recaptcha";
 
 export const Login = () => {
+
+
+  const [captcha, setCaptcha] = React.useState(false);
+
+  const handleCaptcha = () => {
+    setCaptcha(true);
+  }
+
+
   return (
     <div className="login">
       <p className="head">Rent . Ride . Explore</p>
 
       <div className="box">
-
         <div className="flex">
           <p>LOGIN</p>
-          <p><Link to="/register" style={{textDecoration:'none',color:'black'}}>SIGN UP</Link></p>
+          <p>
+            <Link
+              to="/register"
+              style={{ textDecoration: "none", color: "black" }}
+            >
+              SIGN UP
+            </Link>
+          </p>
         </div>
 
         <p className="ph">Phone</p>
@@ -19,21 +35,24 @@ export const Login = () => {
           <input type="number" placeholder="+91" />
           <input type="number" />
         </div>
-        
-        <input className="inp" type="password" placeholder="Password"/>
+
+        <input className="inp" type="password" placeholder="Password" />
 
         <div className="forget">
           <div>
-            <input type="checkbox"/> Remember password?
+            <input type="checkbox" /> Remember password?
           </div>
           <a href="##">Forgot password?</a>
         </div>
 
+        {/* catcha work is pending */}
+
+        <ReCAPTCHA sitekey="Your client site key" onChange={handleCaptcha} />
+
         <button className="btn loginbutton">Login with Password</button>
         <p>OR</p>
         <button className="loginbutton">Login with OTP</button>
-
       </div>
     </div>
-  )
+  );
 }
