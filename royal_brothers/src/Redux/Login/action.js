74551cb9a63bb0f2c848b04f1639f8827aa706d6
divyansh <sikarwar.divyansh.13@gmail.com>
2022-04-02 13:55:53 +0530

    redux work still pending created action & reducer

--- royal_brothers/src/Redux/Login/action.js
+++ royal_brothers/src/Redux/Login/action.js
@@ -1,7 +1,22 @@
 // action types
 
-
-
-
+export const LOGIN_REQUEST = 'LOGIN_REQUEST';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
 
 // action creators
+
+export const loginReques = ({phone, password}) => ({
+    type: LOGIN_REQUEST,
+    payload: {phone, password}
+});
+
+export const loginSuccess = (data) => ({
+    type: LOGIN_SUCCESS,
+    payload: data
+});
+
+export const loginFailure = (error) => ({
+    type: LOGIN_FAILURE,
+    payload: error
+});
--- royal_brothers/src/Redux/Login/loginReducer.js
+++ royal_brothers/src/Redux/Login/loginReducer.js
@@ -1,1 +1,37 @@
 // login reducer
+
+import { LOGIN_FAILURE, LOGIN_REQUEST,LOGIN_SUCCESS } from "./action";
+
+const initialState = {
+    isLoading: false,
+    isError: false,
+    data: null
+}
+
+export const loginReducer = (state = {}, action) => {
+    switch (action.type) {
+        case LOGIN_REQUEST:
+        return {
+            ...state,
+            isLoading: true,
+            isError: false,
+            data: action.payload
+        };
+        case LOGIN_SUCCESS:
+        return {
+            ...state,
+            isLoading: false,
+            isError: false,
+            data: action.payload
+        };
+        case LOGIN_FAILURE:
+        return {
+            ...state,
+            isLoading: false,
+            isError: true,
+            data: action.payload
+        };
+        default:
+        return state;
+    }
+}
