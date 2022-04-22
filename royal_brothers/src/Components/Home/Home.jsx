a288b4366ac25e018700a547c555b1598291b74b
Sudhanshu kumar <96163772+sudhanshu7352@users.noreply.github.com>
2022-04-22 22:52:22 +0530

    Update Home.jsx

--- royal_brothers/src/Components/Home/Home.jsx
+++ royal_brothers/src/Components/Home/Home.jsx
@@ -108,7 +108,7 @@ export const Home = () => {
       </div>
       <div>
 
-      <img className="image2" src="https://d36g7qg6pk2cm7.cloudfront.net/assets/long_term/RBX_carousel_one-5e5beb3711f8184ba280e0791b058b8b1b4f03cad772f10b0ab39adfc5d06ec5.jpg" alt="" />
+
       </div>
       <div className="our_fleet">
         <h3>
